5b9556a4dfc21fa3770e3f44f29342d355a86971
lnvthang <lnvthang.it@gmail.com>
2024-09-07 22:08:28 +0700

    first commit

---
+++ src/comment.java
@@ -0,0 +1,29 @@
+/**
+ * <h1>Comment for file</h1>
+ * This is the template comment for file and method.
+ *
+ * @author  me
+ * @version 1.0
+ * @since   2024-09-07
+ */
+public class comment {
+
+    /**
+     * This method is used to find average of three integers.
+     * @param msg This is the first parameter to myMethod method.
+     * @return String This returns message.
+     */
+    public static String myMethod(String msg) {
+        return msg;
+    }
+
+    /**
+     * This is the main method.
+     * @param args Unused.
+     * @return Nothing.
+     */
+    public static void main(String[] args) {
+        String reuslt = myMethod("Hello Java");
+        System.out.println(reuslt);
+    }
+}
